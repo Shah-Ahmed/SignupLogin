470f97cefb23e3f7c9796bfed02d34fb107c1f63
SHAH-AHMED <shahrajpu2019@gmail.com>
2024-12-17 11:03:10 +0500

    6th

--- app/component/signup/page.jsx
+++ app/component/signup/page.jsx
@@ -47,6 +47,7 @@ export default function Signup() {
           </button>
         </form>
         <p className="text-sm text-center text-gray-300">
+           {/* eslint-disable-next-line react/no-unescaped-entities */}
           Already have an account?{" "}
           <Link href="/" className="font-semibold text-pink-400 hover:underline">
             Login
